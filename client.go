9d7cb77f3c8267dc0fdc34a032413451492cf8d9
Tim Heckman <t@heckman.io>
2017-12-11 00:28:20 -0800

    bump version to v0.2.0
    Signed-off-by: Tim Heckman <t@heckman.io>

--- client.go
+++ client.go
@@ -16,7 +16,7 @@ import (
 )
 
 // Version is the package version
-const Version = "0.1.0"
+const Version = "0.2.0"
 
 // fqpn is the Fully Qualified Package Name for use in the client's User-Agent
 const fqpn = "github.com/theckman/go-ipdata"
