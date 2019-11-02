04b3a360fd5ff41432e16e662c0274cb84929cbb
Tim Heckman <t@heckman.io>
2019-11-02 23:12:49 +1100

    Add missing test coverage around the Error type

--- errors_test.go
+++ errors_test.go
@@ -8,18 +8,24 @@ import "testing"
 
 func TestError(t *testing.T) {
 	const wantStr = "test"
-	const wantInt = 42
+	const wantCode = 42
+	const wantIndex = 84
 
 	ev := Error{
 		m: "test",
 		c: 42,
+		i: 84,
 	}
 
 	if got := ev.Error(); got != wantStr {
 		t.Fatalf("ev.Error() = %q, want %q", got, wantStr)
 	}
 
-	if got := ev.Code(); got != wantInt {
-		t.Fatalf("ev.Code() = %d, want %d", got, wantInt)
+	if got := ev.Code(); got != wantCode {
+		t.Fatalf("ev.Code() = %d, want %d", got, wantCode)
+	}
+
+	if got := ev.Index(); got != wantIndex {
+		t.Fatalf("ev.Index() = %d, want %d", got, wantIndex)
 	}
 }
